5981e60a540eda5caa50481675bbc7f6595759a6
rhunt2911 <huntrachel.work@gmail.com>
2025-11-01 17:13:14 +0000

    created functionality for parsing unique information to modal and allowed for dynamic tag generation based on amount

--- website code/openCard.js
+++ website code/openCard.js
@@ -1,59 +1,82 @@
+//function for displaying modal with correct information
+function displayModal(mID) {
+    //get all modal elements
+    const mTitle= document.querySelector(".modalTitle");
+    //add smth for video when have functionality
+    const mTags = document.querySelector(".modalTag");
+    const mText = document.getElementById("modalText");
+    const mVideo = document.querySelector(".modalVideo");
+    
+    console.log("function called");
+    
+    if(modalObj.hasOwnProperty(mID)) {
+        mTitle.innerHTML = `<h2><i>${modalObj[mID].title}</i></h2>
+                            <a class="modalClose" href="#close" onclick="resetTags()">&times;</a>`;
+        
+        mText.innerHTML = `<p id="modalText"> ${modalObj[mID].text}</p>`;
+
+        for(let i = 0; i < modalObj[mID].tags.length; i++) {
+            let tag = document.createElement("p");
+            tag.innerHTML=`<span>${modalObj[mID].tags[i]}</span>`;
+            mTags.appendChild(tag);
+        };
+        
+
+        console.log("function successful");
+
+    }
+}
+
+//functtion for ing modal tags to allow fresh dynamic generations
+function resetTags(){
+    const mTags = document.querySelector(".modalTag");
+    mTags.innerHTML=``;
+}
+
 //modal information for each portfolio card based on id
 /**
  * object base
  * id: {
-        title: ``,
-        videoId: ``,
-        tag1: ``,
-        tag2: ``,
-        tag3: ``,
-        text: ``,
+        title: "",
+        videoId: "",
+        tags: [
+            "",
+        ]
+        text: "",
     }
  */
+
 const modalObj = {
     ShShTitle: {
-        title: `Shuffle Showdown Title Theme`,
+        title: "Shuffle Showdown Title Theme",
         videoId: `ShTitleVid`,
-        tag1: `logic pro x`,
-        tag2: `fmod`,
-        tag3: `unity`,
+        tags: [
+            "logic pro x",
+            "fmod",
+            "unity"
+        ],
         text: `title screen theme composed for shuffle showdown.`,
     },
 
     cr1: {
         title: `Club Roland: Track 1`,
         videoId: `cr1Vid`,
-        tag1: `logic pro x`,
-        tag2: `fmod`,
-        tag3: `unity`,
+        tags: [
+            "logic pro x",
+            "fmod",
+            "unity"
+        ],
         text: `club roland track 1 for shuffle showdown`,
     },
 
     de1: {
         title: `Disco Express: Track 1`,
         videoId: `de1Vid`,
-        tag1: `logic pro x`,
-        tag2: `fmod`,
-        tag3: `unity`,
+        tags: [
+            "logic pro x",
+            "fmod",
+            "unity"
+        ],
         text: `disco express track 1 for shuffle showdown`,
     }
 };
-
-//get all modal elements
-const mTitle= document.querySelector(".modalTitle");
-//add smth for video when have functionality
-const mTag1 = document.getElementById("modalTag1");
-const mTag2 = document.getElementById("modalTag2");
-const mTag3 = document.getElementById("modalTag1");
-const mText = document.getElementById("modalText");
-
-//function for displaying modal with correct information
-function displayModal(mID) {
-    if(modalObj.hasOwnProperty(mID)) {
-        mTitle.textContent = modalObj[mID].title;
-        mTag1.textContent = modalObj[mID].tag1;
-        mTag2.textContent = modalObj[mID].tag2;
-        mTag3.textContent = modalObj[mID].tag3;
-        mText.textContent = modalObj[mID].text;
-    }
-}
